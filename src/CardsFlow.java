e185a839425aa1be5b9d7841da88f8a8fb4ffa32
kamil <kamil.partyka1998@gmail.com>
2022-04-25 22:47:23 +0200

    Added new class decided on card's flow between players

---
+++ src/CardsFlow.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class CardsFlow {
+    public CardsFlow(Table table,ReturnPlayerAndCard game, ArrayList<Player> players){
+        this.table=table;
+        this.players=players;
+        this.game=game;
+    };
+    int counterToChangeDirection=1;
+    int cardToTake=0;
+    int i=0;
+    Table table;
+    ReturnPlayerAndCard game;
+    ArrayList<Player> players;
+
+    public void controllCardFlow(){
+
+        Punishment x= new Punishment(true,0,1,false);
+        while(true){
+            System.out.println("On the table is " + game.cardOnTable);
+            System.out.println(i+1+ " player");
+            x=table.letsPlay(game.cardOnTable,players.get(i).putCard(),players.get(i),x.isFlag(),game.card,cardToTake);
+            cardToTake=x.getCardToTake();
+            counterToChangeDirection+= x.getDirection();
+
+
+            if(counterToChangeDirection%2==0){
+                --i;
+                if(i<0){
+                    i=2;
+                }
+            }
+            else{
+                ++i;
+                if(i>2){
+                    i=0;
+                }
+            }
+            if(x.isOver()){
+                System.out.println("Player" + i +" Win");
+                break;
+
+            }
+
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -24,43 +24,44 @@ public class Main {
         ArrayList<Card> karty=deck.createDeck();
 
         karty=deck.shuffleCard(karty);
-
         game= table.dealTheCards(peopleCount,karty,players);
         System.out.println("First player's card is "+game.players.get(0).getCardsInHand());
         System.out.println("Second player's card is "+game.players.get(1).getCardsInHand());
         System.out.println("Third player's card is "+game.players.get(2).getCardsInHand());
-int counterToChangeDirection=1;
-int cardToTake=0;
-        int i=0;
-        Punishment x= new Punishment(true,0,1,false);
-        while(true){
-            System.out.println("On the table is " + game.cardOnTable);
-            System.out.println(i+1+ " player");
-            x=table.letsPlay(game.cardOnTable,players.get(i).putCard(),players.get(i),x.isFlag(),game.card,cardToTake);
-            cardToTake=x.getCardToTake();
-            counterToChangeDirection+= x.getDirection();
 
+//int counterToChangeDirection=1;
+//int cardToTake=0;
+//        int i=0;
+//        Punishment x= new Punishment(true,0,1,false);
+//        while(true){
+//            System.out.println("On the table is " + game.cardOnTable);
+//            System.out.println(i+1+ " player");
+//            x=table.letsPlay(game.cardOnTable,players.get(i).putCard(),players.get(i),x.isFlag(),game.card,cardToTake);
+//            cardToTake=x.getCardToTake();
+//            counterToChangeDirection+= x.getDirection();
+//
+//
+//            if(counterToChangeDirection%2==0){
+//                --i;
+//                if(i<0){
+//                    i=2;
+//                }
+//            }
+//            else{
+//                ++i;
+//                if(i>2){
+//                    i=0;
+//                }
+//            }
+//            if(x.isOver()){
+//                System.out.println("Player" + i+1 +" Win");
+//                break;
+//
+//            }
+//
+//            }
 
-            if(counterToChangeDirection%2==0){
-                --i;
-                if(i<0){
-                    i=2;
-                }
-            }
-            else{
-                ++i;
-                if(i>2){
-                    i=0;
-                }
-            }
-            if(x.isOver()){
-                System.out.println("Player" + i+1 +" Win");
-                break;
-
-            }
-
-            }
-
-
+CardsFlow cardsFlow= new CardsFlow(table,game,players);
+cardsFlow.controllCardFlow();
     }
 }
